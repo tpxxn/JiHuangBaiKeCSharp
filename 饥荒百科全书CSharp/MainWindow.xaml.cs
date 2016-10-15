e2e3507e9df161e46897cd27f7b4c243547afd2c
tpxxn <351765204@qq.com>
2016-10-16 01:24:45 +0800

    移动窗口

--- 饥荒百科全书CSharp/MainWindow.xaml.cs
+++ 饥荒百科全书CSharp/MainWindow.xaml.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,17 +11,41 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
 namespace 饥荒百科全书CSharp
 {
-    /// <summary>
-    /// MainWindow.xaml 的交互逻辑
-    /// </summary>
+    /// <summary>  
+    /// MainWindow.xaml 的交互逻辑  
+    /// </summary>  
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
         }
+        private static bool IsDrag = false;
+        private double enterX;
+        private double enterY;
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            IsDrag = true;
+            enterX = e.GetPosition(this).X;
+            enterY = e.GetPosition(this).Y;
+        }
+
+        private void Window_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            IsDrag = false;
+            enterX = 0;
+            enterY = 0;
+        }
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (IsDrag)
+            {
+                this.Left += e.GetPosition(this).X - enterX;
+                this.Top += e.GetPosition(this).Y - enterY;
+            }
+        }
+
     }
 }
