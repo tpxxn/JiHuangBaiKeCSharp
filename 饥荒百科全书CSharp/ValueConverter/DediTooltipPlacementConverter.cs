650a4b522e845de022c9758a56af26c2d206e30a
tpxxn <351765204@qq.com>
2016-12-06 01:45:11 +0800

    MainPanel测试4(Tooltip居中/值转换器)

---
+++ 饥荒百科全书CSharp/ValueConverter/DediTooltipPlacementConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace 饥荒百科全书CSharp.ValueConverter
+{
+    class DediTooltipPlacementConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double width = (double)value;
+            if (width >= 50)
+            {
+                return -(width - 50) / 2 + 2;
+            }
+            else
+            {
+                return (50 - width) / 2 + 2;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
