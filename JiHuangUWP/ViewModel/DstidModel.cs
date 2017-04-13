6b99c8368d6a691b64f8b9988e46bf7703c7aced
lindexi <lindexi@cvte.com>
2017-04-13 13:56:44 +0800

    feat:不在页面进行

--- JiHuangUWP/ViewModel/DstidModel.cs
+++ JiHuangUWP/ViewModel/DstidModel.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using JiHuangUWP.Model;
 
@@ -63,6 +64,8 @@ namespace JiHuangUWP.ViewModel
         {
             int i = 0;
             bool reu = false;
+            text = text.ToLower();
+            str = str.ToLower();
             foreach (var temp in str)
             {
                 reu = false;
@@ -71,6 +74,7 @@ namespace JiHuangUWP.ViewModel
                     if (temp == text[i])
                     {
                         reu = true;
+                        i++;
                         break;
                     }
                 }
@@ -85,7 +89,7 @@ namespace JiHuangUWP.ViewModel
 
         public override async void OnNavigatedTo(object obj)
         {
-            _dstidAbigail = obj as List<DstidAbigail>;
+            //_dstidAbigail = obj as List<DstidAbigail>;
             if (_dstidAbigail != null)
             {
                 DstidAbigail.Clear();
@@ -96,9 +100,70 @@ namespace JiHuangUWP.ViewModel
 
                 foreach (var temp in _dstidAbigail)
                 {
+                    //image
                     await temp.Read();
                 }
             }
+        }
+
+        public async Task Read()
+        {
+            List<DstidAbigail> dstidAbigail = new List<DstidAbigail>();
+            try
+            {
+                string url = "ms-appx:///GameResource/DstidAbigail.txt";
+                StorageFile file = await StorageFile.GetFileFromApplicationUriAsync(new Uri(url));
+
+                StorageFolder folder = await file.GetParentAsync();
+
+                var str = (await FileIO.ReadTextAsync(file)).Split('\n');
+                for (int i = 0; i < str.Length; i++)
+                {
+                    dstidAbigail.Add(new DstidAbigail()
+                    {
+                        Name = str[i++].Trim(),
+                        Chinese = str[i++].Trim(),
+                        Debug = str[i++].Trim(),
+                        Image = str[i++].Trim()
+                    });
+                }
+
+                DsBitmapRead(dstidAbigail, await folder.GetFilesAsync());
+
+            }
+            catch (Exception)
+            {
+
+            }
+
+            //return dstidAbigail;
+            _dstidAbigail = dstidAbigail;
+        }
+
+        private static void DsBitmapRead(List<DstidAbigail> dstidAbigail, IReadOnlyList<StorageFile> folder)
+        {
+            //foreach (var temp in await folder)
+            //{
+            //    for (int i = 0; i < dstidAbigail.Count; i++)
+            //    {
+            //        string str = dstidAbigail[i].Name.Replace(" ", "_");
+
+            //    }
+            //}
+
+
+            foreach (var temp in dstidAbigail)
+            {
+                for (int i = 0; i < folder.Count; i++)
+                {
+                    string str = temp.Name.Replace(" ", "_").ToLower();
+                    if (folder[i].Name.Substring(2).ToLower() == str + ".png")
+                    {
+                        temp.Image = folder[i].Name;
+                    }
+                }
+            }
+
         }
     }
 }
--- JiHuangUWP/ViewModel/ViewModel.cs
+++ JiHuangUWP/ViewModel/ViewModel.cs
@@ -56,62 +56,14 @@ namespace JiHuangUWP.ViewModel
                 ViewModel = DstidModel
             });
 
-            List<DstidAbigail> dstidAbigail = new List<DstidAbigail>();
-            try
-            {
-                string url = "ms-appx:///GameResource/DstidAbigail.txt";
-                StorageFile file = await StorageFile.GetFileFromApplicationUriAsync(new Uri(url));
+            await DstidModel.Read();
 
-                StorageFolder folder = await file.GetParentAsync();
-
-                var str = (await FileIO.ReadTextAsync(file)).Split('\n');
-                for (int i = 0; i < str.Length; i++)
-                {
-                    dstidAbigail.Add(new DstidAbigail()
-                    {
-                        Name = str[i++].Trim(),
-                        Chinese = str[i++].Trim(),
-                        Debug = str[i++].Trim(),
-                        Image = str[i++].Trim()
-                    });
-                }
-
-                DsBitmapRead(dstidAbigail, await folder.GetFilesAsync());
-
-            }
-            catch (Exception)
-            {
-
-            }
-
-            Navigateto(typeof(DstidModel), dstidAbigail);
+            Navigateto(typeof(DstidModel),null);
         }
 
-        private static void DsBitmapRead(List<DstidAbigail> dstidAbigail, IReadOnlyList<StorageFile> folder)
-        {
-            //foreach (var temp in await folder)
-            //{
-            //    for (int i = 0; i < dstidAbigail.Count; i++)
-            //    {
-            //        string str = dstidAbigail[i].Name.Replace(" ", "_");
+        
 
-            //    }
-            //}
-
-
-            foreach (var temp in dstidAbigail)
-            {
-                for (int i = 0; i < folder.Count; i++)
-                {
-                    string str = temp.Name.Replace(" ", "_").ToLower();
-                    if (folder[i].Name.Substring(2).ToLower() == str + ".png")
-                    {
-                        temp.Image = folder[i].Name;
-                    }
-                }
-            }
-
-        }
+    
 
         public ViewModel View
         {
